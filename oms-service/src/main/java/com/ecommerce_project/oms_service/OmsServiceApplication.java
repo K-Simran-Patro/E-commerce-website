92d4c995f529ad07edc828cfd8ba86755393dce8
K-Simran-Patro <144943688+K-Simran-Patro@users.noreply.github.com>
2026-04-30 15:50:51 +0530

    project structure

---
+++ oms-service/src/main/java/com/ecommerce_project/oms_service/OmsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce_project.oms_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmsServiceApplication.class, args);
+	}
+
+}
---
+++ wishlist-service/src/test/java/com/ecommerce_project/wishlist_service/WishlistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce_project.wishlist_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
